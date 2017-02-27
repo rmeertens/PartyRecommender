cb4dbf3e18c01414b6a21afee8efd9cf4e24037c
Roland Meertens <rolandmeertens@gmail.com>
2017-02-27 21:55:26 +0100

    tweaking

--- model.py
+++ model.py
@@ -5,14 +5,14 @@ from keras.layers import LSTM
 from keras.layers.embeddings import Embedding
 from keras.preprocessing import sequence
 from keras.utils import np_utils
-from keras.models import load_model
+import keras.models
 
 import numpy as np
 
 def get_model(words_top_count,embedding_vector_length,max_review_length,party_count):
     model = Sequential()
     model.add(Embedding(words_top_count, embedding_vector_length, input_length=max_review_length))
-    model.add(LSTM(100))
+    model.add(LSTM(300))
     model.add(Dense(party_count, activation='softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['categorical_accuracy'])
     return model
@@ -21,7 +21,7 @@ def train_model(model,X_train,y_train,epochs=3,batch_size=64):
     model.fit(X_train, y_train, nb_epoch=epochs, batch_size=batch_size, verbose=1)
 
 def load_model(filename):
-    model = load_model(filename)
+    model = keras.models.load_model(filename)
     return model
 
 
