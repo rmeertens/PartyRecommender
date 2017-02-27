68df23c2ed46aab92d2d037ab74a1a856c5ad2cb
Roland Meertens <rolandmeertens@gmail.com>
2017-02-27 22:20:40 +0100

    pickling data

--- main.py
+++ main.py
@@ -1,32 +1,45 @@
 
-import model
 import os
-import partyprogram_loader
+import pickle
+
 from keras.preprocessing import sequence
 from keras.utils import np_utils
 
+import model
+import partyprogram_loader
+
 # GLOBALS YOU CAN SET AND PLAY WITH
 FILENAME_SAVED_MODEL = 'party_model.h5'
+FILENAME_SAVED_DATA = 'data.p'
+
 PARTIJPATH = 'partijprogrammas'
 EMBEDDING_VECTOR_LENGTH = 32
 MAX_REVIEW_LENGTH = 20
 
 if __name__ == "__main__":
+    if os.path.exists(FILENAME_SAVED_DATA):
+        loaded = pickle.load(open(FILENAME_SAVED_DATA, "rb"))
+        X_test = loaded['X_test']
+        y_test = loaded['y_test']
+    else:
 
-    parties_and_sentences= partyprogram_loader.get_parties_and_sentences(PARTIJPATH)
-    vocab_list = partyprogram_loader.vocab_from_sentences(parties_and_sentences)
-    id_of_word_getter = partyprogram_loader.IdOfWordGetter(vocab_list)
-    party_count = len(parties_and_sentences.keys())
-    word_count = len(vocab_list)
+        parties_and_sentences= partyprogram_loader.get_parties_and_sentences(PARTIJPATH)
+        vocab_list = partyprogram_loader.vocab_from_sentences(parties_and_sentences)
+        id_of_word_getter = partyprogram_loader.IdOfWordGetter(vocab_list)
+        party_count = len(parties_and_sentences.keys())
+        word_count = len(vocab_list)
 
-    (X_train, y_train), (X_test, y_test) = partyprogram_loader.load_data(parties_and_sentences, id_of_word_getter)
+        (X_train, y_train), (X_test, y_test) = partyprogram_loader.load_data(parties_and_sentences, id_of_word_getter)
 
-    X_train = sequence.pad_sequences(X_train, maxlen=MAX_REVIEW_LENGTH)
-    X_test = sequence.pad_sequences(X_test, maxlen=MAX_REVIEW_LENGTH)
-    y_train = np_utils.to_categorical(y_train, party_count)
-    prevytest = y_test
-    y_test = np_utils.to_categorical(y_test, party_count)
-
+        X_train = sequence.pad_sequences(X_train, maxlen=MAX_REVIEW_LENGTH)
+        X_test = sequence.pad_sequences(X_test, maxlen=MAX_REVIEW_LENGTH)
+        y_train = np_utils.to_categorical(y_train, party_count)
+        prevytest = y_test
+        y_test = np_utils.to_categorical(y_test, party_count)
+        tosave = dict()
+        tosave['y_test'] = y_test
+        tosave['X_test'] = X_test
+        pickle.dump(tosave, open(FILENAME_SAVED_DATA, "wb"))
 
     if os.path.exists(FILENAME_SAVED_MODEL):
         kerasmodel = model.load_model(FILENAME_SAVED_MODEL)
