1bf994a61d295ec079d57fe8b1830593fd0b9aae
Roland Meertens <rolandmeertens@gmail.com>
2017-03-06 21:49:34 +0100

    adding right header

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ def predict_party():
 @app.after_request
 def after_request(response):
   response.headers.add('Access-Control-Allow-Origin', '*')
-  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,predicttext')
   response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
   return response
 
